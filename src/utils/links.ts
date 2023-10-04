eeeecb3a62fa859895a952771dd2c38ddd27cf2f
Jonas Brauer <jonasbrauer@seznam.cz>
2023-10-04 12:18:23 +0200

    feat: updated detail page, fixes

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,11 +1,12 @@
-//todo: dotenv
-export const BASE_URL = "https://chef-a2nlxswxda-ez.a.run.app"
-// export const BASE_URL = "http://localhost:8000"
+export const BASE_URL = import.meta.env.API_URL
+export const IMAGES_URL = import.meta.env.IMAGES_URL
 
 export const smallImage = (id: number) => {
+    return `${IMAGES_URL}/${id}/small.jpeg`
     return `${BASE_URL}/images/${id}/small.jpeg`
 }
 
 export const mediumImage = (id: number) => {
+    return `${IMAGES_URL}/${id}/medium.jpeg`
     return `${BASE_URL}/images/${id}/medium.jpeg`
 }
